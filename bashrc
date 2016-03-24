5611eb64e1457dd6dc39dd3b0daae5c6544e372d
Andrew Leedom <andrew.leedom@addteq.com>
2016-03-24 10:26:58 -0400

    add script to shorten path when num dirs > 2

--- bashrc
+++ bashrc
@@ -124,6 +124,8 @@ export GIT_PS1_SHOWDIRTYSTATE=1
 # '\u' adds the name of the current user to the prompt
 # '\$(__git_ps1)' adds git-related stuff
 # '\W' adds the name of the current directory
-export PS1="$purple\u$blue@$purple\h$green\$(__git_ps1)$blue \w $ $reset"
+# '\h adds the current hostname
+export PS1="$purple\u$blue@$purple\h$green\$(__git_ps1)$blue\$(python ~/dotfiles/path.py)$ $reset"
 
 source /usr/local/bin/virtualenvwrapper.sh
+
---
+++ path.py
@@ -0,0 +1,17 @@
+import os
+
+
+from commands import getoutput
+from socket import gethostname
+hostname = gethostname()
+username = os.environ['USER']
+pwd = os.getcwd()
+homedir = os.path.expanduser('~')
+pwd = pwd.replace(homedir, '~', 1)
+path_dirs = pwd.split('/')
+if os.path.isabs(pwd):
+    path_dirs[1] = "/"+path_dirs[1]
+    path_dirs = path_dirs[1:]
+if len(path_dirs) > 2:
+    pwd = "{}/ ... /{}".format(path_dirs[0], path_dirs[-1])
+print(pwd)
